eabe628b8244e79c6b95c64a00f78d00d999d68a
Gizmo <nalanbilgin@gmail.com>
2024-11-02 16:09:09 +0200

    Added url.py template and view for flower list

---
+++ thesecretgarden/thesecretgarden/flowers/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from thesecretgarden.flowers.views import show_flowers_list
+
+urlpatterns = [
+    # path('', FlowersListView.as_view(), name='flowers-list'),
+    path('', show_flowers_list(), name='flowers-list'),
+]
--- thesecretgarden/thesecretgarden/flowers/views.py
+++ thesecretgarden/thesecretgarden/flowers/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+def show_flowers_list(request):
+    return render(request, 'flowers/flowers-list.html')
+# class FlowersListView(ListView):
+#     template_name = 'flowers/flowers-list.html'
+#
+
