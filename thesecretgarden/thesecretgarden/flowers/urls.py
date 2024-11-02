d7aad833c9a61eb45bc7eb112b2eda66dca20e94
Gizmo <nalanbilgin@gmail.com>
2024-11-02 16:10:22 +0200

    Corrected mistake in flowers urls.py

--- thesecretgarden/thesecretgarden/flowers/urls.py
+++ thesecretgarden/thesecretgarden/flowers/urls.py
@@ -4,5 +4,5 @@ from thesecretgarden.flowers.views import show_flowers_list
 
 urlpatterns = [
     # path('', FlowersListView.as_view(), name='flowers-list'),
-    path('', show_flowers_list(), name='flowers-list'),
+    path('', show_flowers_list, name='flowers-list'),
 ]
