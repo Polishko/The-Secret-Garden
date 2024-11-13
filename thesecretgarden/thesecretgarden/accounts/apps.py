55ac0d321e039a13ab5c5305e93701ad9805377f
Gizmo <nalanbilgin@gmail.com>
2024-11-13 17:12:06 +0200

    Feature: Added user creation signal

--- thesecretgarden/thesecretgarden/accounts/apps.py
+++ thesecretgarden/thesecretgarden/accounts/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'thesecretgarden.accounts'
+
+    def ready(self):
+        import thesecretgarden.accounts.signals
+        
