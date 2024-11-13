5bec6721ab53d6d4f361d2255ac887f34787d190
Gizmo <nalanbilgin@gmail.com>
2024-11-13 13:45:22 +0200

    Upgrade: Updated settings

--- thesecretgarden/thesecretgarden/settings.py
+++ thesecretgarden/thesecretgarden/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 MY_APPS = [
     'thesecretgarden.common',
     'thesecretgarden.flowers',
+    'thesecretgarden.accounts',
 ]
 
 THIRD_PARTY_APPS = [
@@ -148,3 +149,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.AppUser'
