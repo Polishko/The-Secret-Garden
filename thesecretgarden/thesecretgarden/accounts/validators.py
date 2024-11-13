cec2077414d45de9c45188ff8eeffbeebaca77e4
Gizmo <nalanbilgin@gmail.com>
2024-11-13 17:12:58 +0200

    Added validator for Profile

--- thesecretgarden/thesecretgarden/accounts/validators.py
+++ thesecretgarden/thesecretgarden/accounts/validators.py
@@ -11,3 +11,15 @@ class UsernameValidator:
     def __call__(self, value):
         if not value.isalnum():
             raise ValidationError(self.message)
+
+
+@deconstructible
+class NameValidator:
+    def __init__(self, field_name, message=None):
+        self.field_name = field_name
+        self.message = message or f'Your {self.field_name} must contain only letters!'
+
+
+    def __call__(self, value):
+        if not value.isalpha():
+            raise ValidationError(self.message)
