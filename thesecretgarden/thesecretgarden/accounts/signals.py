a7f26800cab1fb3ca31c8a44f88e1513db0ca8f8
Gizmo <nalanbilgin@gmail.com>
2024-11-16 17:32:36 +0200

    Refine: Refined code for profile creation signal

--- thesecretgarden/thesecretgarden/accounts/signals.py
+++ thesecretgarden/thesecretgarden/accounts/signals.py
@@ -1,14 +1,16 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from thesecretgarden.accounts.models import Profile
+
 
 UserModel = get_user_model()
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=UserModel)
 def create_profile(sender, instance, created, **kwargs):
+    """
+    Signal to create a Profile when a new User is created.
+    """
     if created:
         Profile.objects.create(
             user=instance,
