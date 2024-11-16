ff67ae09350fbf6619cc68519068c49df9314c8e
Gizmo <nalanbilgin@gmail.com>
2024-11-16 16:21:52 +0200

    Upgrade: User manager updated to add default role customer upon user creation

--- thesecretgarden/thesecretgarden/accounts/managers.py
+++ thesecretgarden/thesecretgarden/accounts/managers.py
@@ -2,14 +2,29 @@ from django.contrib import auth
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.hashers import make_password
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class AppUserManager(BaseUserManager):
+    """
+    Internal helper method to create and save a regular user with the given username, email, and password.
+    This method enforces default roles for all users.
+    """
     def _create_user(self, username, email, password, **extra_fields):
         if not username:
             raise ValueError("The given username must be set")
         email = self.normalize_email(email)
-
         username = self.model.normalize_username(username)
+
+        """
+        Forcing default role for all users created outside the admin panel
+        """
+        extra_fields.setdefault('role', 'customer')
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        logger.info(f"Creating user with username: {username}, email: {email}")
 
         user = self.model(username=username, email=email, **extra_fields)
         user.password = make_password(password)
@@ -17,11 +32,16 @@ class AppUserManager(BaseUserManager):
         return user
 
     def create_user(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
+        """
+        Create and return a regular user, role customer with the given username, email, and password.
+        """
         return self._create_user(username, email, password, **extra_fields)
 
     def create_superuser(self, username, email=None, password=None, **extra_fields):
+        """
+        Create and return a superuser, role admin with the given username, email, and password.
+        """
+        extra_fields.setdefault('role', 'admin')
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -32,29 +52,29 @@ class AppUserManager(BaseUserManager):
 
         return self._create_user(username, email, password, **extra_fields)
 
-    def with_perm(
-        self, perm, is_active=True, include_superusers=True, backend=None, obj=None
-    ):
-        if backend is None:
-            backends = auth._get_backends(return_tuples=True)
-            if len(backends) == 1:
-                backend, _ = backends[0]
-            else:
-                raise ValueError(
-                    "You have multiple authentication backends configured and "
-                    "therefore must provide the `backend` argument."
-                )
-        elif not isinstance(backend, str):
-            raise TypeError(
-                "backend must be a dotted import path string (got %r)." % backend
-            )
-        else:
-            backend = auth.load_backend(backend)
-        if hasattr(backend, "with_perm"):
-            return backend.with_perm(
-                perm,
-                is_active=is_active,
-                include_superusers=include_superusers,
-                obj=obj,
-            )
-        return self.none()
+    # def with_perm(
+    #     self, perm, is_active=True, include_superusers=True, backend=None, obj=None
+    # ):
+    #     if backend is None:
+    #         backends = auth._get_backends(return_tuples=True)
+    #         if len(backends) == 1:
+    #             backend, _ = backends[0]
+    #         else:
+    #             raise ValueError(
+    #                 "You have multiple authentication backends configured and "
+    #                 "therefore must provide the `backend` argument."
+    #             )
+    #     elif not isinstance(backend, str):
+    #         raise TypeError(
+    #             "backend must be a dotted import path string (got %r)." % backend
+    #         )
+    #     else:
+    #         backend = auth.load_backend(backend)
+    #     if hasattr(backend, "with_perm"):
+    #         return backend.with_perm(
+    #             perm,
+    #             is_active=is_active,
+    #             include_superusers=include_superusers,
+    #             obj=obj,
+    #         )
+    #     return self.none()
