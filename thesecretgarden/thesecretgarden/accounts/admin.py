779d1c28ee27735eccaff91134853772d0d552ba
Gizmo <nalanbilgin@gmail.com>
2024-11-16 17:28:22 +0200

    Upgrade: Removed ProfileInline from admin: conflicting with Profile creation signal

--- thesecretgarden/thesecretgarden/accounts/admin.py
+++ thesecretgarden/thesecretgarden/accounts/admin.py
@@ -1,21 +1,19 @@
 from django.contrib import admin
-from django.contrib.admin import StackedInline
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from thesecretgarden.accounts.forms import AppUserChangeForm, AppUserForm
-from thesecretgarden.accounts.models import Profile
 
 UserModel = get_user_model()
 
-class ProfileInline(StackedInline):
-    model = Profile
-    can_delete = False
-    fields = ('first_name', 'last_name', 'preferred_flower_type',)
-
 @admin.register(UserModel)
 class AppUserAdmin(UserAdmin):
-    inlines = (ProfileInline,)
+    ROLE_PERMISSIONS = {
+        'customer': {'is_staff': False, 'is_superuser': False},
+        'staff': {'is_staff': True, 'is_superuser': False},
+        'admin': {'is_staff': True, 'is_superuser': True},
+    }
+
     add_form = AppUserForm
     form = AppUserChangeForm
 
@@ -26,18 +24,36 @@ class AppUserAdmin(UserAdmin):
 
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal Info', {'fields': ('email', 'uuid')}),
+        ('Personal Info', {'fields': ('email', 'uuid')}),  # Profile fields removed
         ('Permissions', {'fields': ('role', 'is_staff', 'is_active', 'is_superuser')}),
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2'),
+            'fields': ('username', 'email', 'password1', 'password2', 'role'),
         }),
     )
 
     def get_readonly_fields(self, request, obj=None):
+        """
+        No one can edit uuid, users other than superusers cannot edit email, username, role.
+        """
         if not request.user.is_superuser:
             return ['role', 'username', 'email']
         return ['uuid']
+
+    def update_permissions_based_on_role(self, obj):
+        """
+        Update is_staff and is_superuser based on role using the ROLE_PERMISSIONS dict.
+        """
+        role_permissions = self.ROLE_PERMISSIONS.get(obj.role, {'is_staff': False, 'is_superuser': False})
+        obj.is_staff = role_permissions['is_staff']
+        obj.is_superuser = role_permissions['is_superuser']
+
+    def save_model(self, request, obj, form, change):
+        """
+        Automatically update is_staff and is_superuser based on role.
+        """
+        self.update_permissions_based_on_role(obj)
+        super().save_model(request, obj, form, change)
