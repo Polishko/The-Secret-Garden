97bdaa0123e384257379e42e51c278b58945ddd3
Gizmo <nalanbilgin@gmail.com>
2024-11-17 14:11:22 +0200

    Update: New validators for birthday, address, phone number fields

--- thesecretgarden/thesecretgarden/accounts/validators.py
+++ thesecretgarden/thesecretgarden/accounts/validators.py
@@ -1,3 +1,6 @@
+import re
+from datetime import date
+
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 
@@ -23,3 +26,37 @@ class NameValidator:
     def __call__(self, value):
         if not value.isalpha():
             raise ValidationError(self.message)
+
+
+@deconstructible
+class PhoneNumberValidator:
+    def __init__(self, message=None):
+        self.message = message or 'Enter a valid phone number of 10 digits!'
+
+    def __call__(self, value):
+        if not re.match(r'^0\d{9}$', value):
+            raise ValidationError(self.message)
+
+
+@deconstructible
+class AddressValidator:
+    def __init__(self, message=None):
+        self.message = message or 'Address contains invalid characters.'
+
+    def __call__(self, value):
+        if re.search(r'[<>]', value):
+            raise ValidationError(self.message)
+
+
+@deconstructible
+class BirthdayValidator:
+    def __init__(self, min_year=1900, future_error=None, past_error=None):
+        self.min_year = min_year
+        self.future_error = future_error or 'Birthday cannot be after today!'
+        self.past_error = past_error or f'Birthday cannot be before {min_year}!'
+
+    def __call__(self, value):
+        if value > date.today():
+            raise ValidationError(self.future_error)
+        if value.year < self.min_year:
+            raise ValidationError(self.past_error)
