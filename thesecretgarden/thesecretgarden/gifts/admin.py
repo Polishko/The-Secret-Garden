d75b6c0802aa42aa8948ec61341a192a0fdd78f5
Gizmo <nalanbilgin@gmail.com>
2024-11-18 20:59:45 +0200

    Feat: Registered Gift model to admin

--- thesecretgarden/thesecretgarden/gifts/admin.py
+++ thesecretgarden/thesecretgarden/gifts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from thesecretgarden.gifts.models import Gift
+from thesecretgarden.mixins import StockManagementAdminMixin
+
+
+@admin.register(Gift)
+class GiftAdmin(admin.ModelAdmin, StockManagementAdminMixin):
+
+    list_display = ('brand_name', 'short_name', 'short_description', 'type', 'price', 'stock', 'slug')
+    list_filter = ('type', 'price', 'stock')
+    search_fields = ('brand_name', 'short_name')
+    ordering = ('brand_name',)
+    readonly_fields = ('slug',)
+
+    fieldsets = (
+        (None, {'fields': ('brand_name', 'type', 'slug')}),
+        ('Details', {'fields': ('short_name', 'short_description', 'price', 'stock', 'photo')}),
+    )
+
+    actions = ['mark_as_out_of_stock']
