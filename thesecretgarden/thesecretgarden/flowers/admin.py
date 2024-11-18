20213229f262f981bc6389bbcddd994fa20c391c
Gizmo <nalanbilgin@gmail.com>
2024-11-18 16:58:29 +0200

    Feat: Registered Plant model to admin

--- thesecretgarden/thesecretgarden/flowers/admin.py
+++ thesecretgarden/thesecretgarden/flowers/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from thesecretgarden.flowers.models import Plant
+
+
+@admin.action(description='Mark as out of stock')
+def mark_as_out_of_stock(model, request, queryset):
+    queryset.update(stock=0)
+
+
+@admin.register(Plant)
+class PlantAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'type', 'price', 'stock', 'slug')
+    list_filter = ('type', 'price', 'stock')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
+    readonly_fields = ('slug',)
+
+    fieldsets = (
+        (None, {'fields': ('name', 'type', 'slug')}),
+        ('Details', {'fields': ('description', 'price', 'stock', 'photo')}),
+    )
+
+    actions = [mark_as_out_of_stock]
