3fed45dc028fa2bdc996d8f5ff69179762bcc447
Gizmo <nalanbilgin@gmail.com>
2024-11-20 12:12:25 +0200

    Added: simple generic ListView for plants list

--- thesecretgarden/thesecretgarden/flowers/views.py
+++ thesecretgarden/thesecretgarden/flowers/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
+from thesecretgarden.flowers.models import Plant
+
 
 def show_flowers_list(request):
     return render(request, 'flowers/flowers-list.html')
 
-class FlowersListView(ListView):
+class PlantsListView(ListView):
+    model = Plant
     template_name = 'flowers/flowers-list.html'
+    context_object_name = 'plants'
 
 
