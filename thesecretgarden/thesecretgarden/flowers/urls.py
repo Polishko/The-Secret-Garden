e157374262c811c7b5b86d4d946b555652906f43
Gizmo <nalanbilgin@gmail.com>
2024-11-20 16:39:29 +0200

    Added url for plant bulk creation

--- thesecretgarden/thesecretgarden/flowers/urls.py
+++ thesecretgarden/thesecretgarden/flowers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from thesecretgarden.flowers.views import show_flowers_list
+from thesecretgarden.flowers.views import show_flowers_list, BulkCreatePlantView
 
 urlpatterns = [
     # path('', FlowersListView.as_view(), name='flowers-list'),
-    path('', show_flowers_list, name='flowers-list'),
+    path('', show_flowers_list, name='plants-list'),
+    path('bulk-create/', BulkCreatePlantView.as_view(), name='plant-bulk-create'),
 ]
