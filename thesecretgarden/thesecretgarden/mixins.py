90bff94932a1d2db7e24b788b6ef82da39147dba
Gizmo <nalanbilgin@gmail.com>
2024-11-21 21:52:58 +0200

    Added: Placeholder mixin for future use

--- thesecretgarden/thesecretgarden/mixins.py
+++ thesecretgarden/thesecretgarden/mixins.py
@@ -5,3 +5,18 @@ class StockManagementAdminMixin:
     @admin.action(description='Mark as out of stock')
     def mark_as_out_of_stock(model, request, queryset):
         queryset.update(stock=0)
+
+
+class PlaceHolderMixin:
+    def add_placeholder(self):
+        if hasattr(self, 'fields'):
+            for field_name, field in self.fields.items():
+                help_text = getattr(field, 'help_text', None)
+                if help_text:
+                    field.widget.attrs.update({
+                        'placeholder': help_text,
+                    })
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_placeholder()
