d4117ef8292404c5e95c6014da56ead6688610b1
Gizmo <nalanbilgin@gmail.com>
2024-11-23 10:31:57 +0200

    Update: Refractured code for moved out common tests

--- thesecretgarden/thesecretgarden/flowers/tests/tests_models.py
+++ thesecretgarden/thesecretgarden/flowers/tests/tests_models.py
@@ -38,63 +38,14 @@ class PlantModelTests(TestCase):
             self.assertIn(f'{self.plant.type} is not in listed gift type choices!',
                              context.exception.message_dict)
 
-    def test_slug_remains_unchanged_on_name_change(self):
-        self.plant.name = 'Bright Pink Roses'
-        self.plant.save()
-        self.assertEqual(self.plant.slug, 'pink-roses')
-
-    def test_name_correctly_capitalized(self):
-        self.plant.name = 'bright pink RoSes'
-        self.plant.save()
-        self.assertEqual(self.plant.name, 'Bright Pink Roses')
-
-    def test_tags_in_description_removed(self):
-        self.plant.description = 'A very romantic present for <script>console.log(Hello there!)</script> for loved ones.'
-        self.plant.save()
-        self.assertEqual(self.plant.description, 'A very romantic present for console.log(Hello there!) for loved ones.')
-
-    def test_duplicate_name_not_allowed(self):
-        Plant.objects.create(
-            name='Bright Pink Roses',
-            type='floral',
-            description='A bright pink rose plant.',
-            price=Decimal('10.99'),
-            stock=10,
-            photo=self.mock_photo
-        )
-
-        with self.assertRaises(ValidationError) as context:
-            Plant.objects.create(
-                name='Bright Pink Roses',
-                type='floral',
-                description='Another bright pink rose plant.',
-                price=Decimal('12.99'),
-                stock=5,
-                photo=self.mock_photo
-            )
-
-        self.assertIn('name', context.exception.message_dict)
-        self.assertIn('slug', context.exception.message_dict)
-
     def test_type_max_length_constraint_applied(self):
         with self.assertRaises(ValidationError):
             self.plant.type = 'Green house plant'
             self.plant.save()
 
-    def test_price_max_digits_constraint_applied(self):
-        with self.assertRaises(ValidationError):
-            Plant.objects.create(
-                name='Blue Orchid',
-                type='floral',
-                description='Amazing blue hues.',
-                price=Decimal('13.99589'),
-                stock=15,
-                photo=self.mock_photo
-            )
 
     def tearDown(self):
         if hasattr(self, 'plant') and self.plant.photo:
             self.plant.photo.delete(save=False)
         if hasattr(self, 'plant'):
             self.plant.delete()
-
--- thesecretgarden/thesecretgarden/flowers/tests/tests_validators.py
+++ thesecretgarden/thesecretgarden/flowers/tests/tests_validators.py
@@ -5,37 +5,7 @@ from django.test import TestCase
 
 from django.core.exceptions import ValidationError
 
-from thesecretgarden.flowers.validators import PlantNameValidator, PlantDescriptionValidator, PlantPriceValidator, \
-    FileSizeValidator, PlantStockValidator
-
-
-class PlantNameValidatorTests(TestCase):
-    def setUp(self):
-        self.validator = PlantNameValidator()
-
-    def test_valid_plant_name(self):
-        try:
-            self.validator('Pink Roses')
-        except ValidationError:
-            self.fail('PlantNameValidator raised ValidationError unexpectedly!')
-
-    def test_invalid_plant_name_1(self):
-        with self.assertRaises(ValidationError) as context:
-            self.validator('Pink_Roses!')
-
-            self.assertEqual(
-                context.exception.message,
-                'Plant name must contain only letters or spaces!'
-            )
-
-    def test_invalid_plant_name_2(self):
-        with self.assertRaises(ValidationError) as context:
-            self.validator('Pinkish Amazing Cool Roses!')
-
-            self.assertEqual(
-                context.exception.message,
-                'Plant name should consist of no more than 3 words!'
-            )
+from thesecretgarden.flowers.validators import PlantDescriptionValidator
 
 
 class PlantDescriptionValidatorTests(TestCase):
@@ -55,79 +25,4 @@ class PlantDescriptionValidatorTests(TestCase):
             self.assertEqual(
                 context.exception.message,
                 'Description cannot contain invalid characters like < or >.'
-            )
-
-
-class PlantPriceValidatorTests(TestCase):
-    def setUp(self):
-        self.validator = PlantPriceValidator()
-
-    def test_valid_price(self):
-        try:
-            self.validator(Decimal(12.56))
-        except ValidationError:
-            self.fail('PlantPriceValidator raised ValidationError unexpectedly!')
-
-    def test_invalid_price_1(self):
-        with self.assertRaises(ValidationError) as context:
-            self.validator(0)
-
-            self.assertEqual(
-                context.exception.message,
-                'Price must be a positive number!'
-            )
-
-    def test_invalid_price_2(self):
-        with self.assertRaises(ValidationError):
-            self.validator(-1.22)
-
-    def test_invalid_price_3(self):
-        with self.assertRaises(ValidationError):
-            self.validator(1000)
-
-
-class PlantStockValidatorTests(TestCase):
-    def setUp(self):
-        self.validator = PlantStockValidator()
-
-    def test_valid_stock(self):
-        try:
-            self.validator(100)
-        except ValidationError:
-            self.fail('PlantStockValidator raised ValidationError unexpectedly!')
-
-    def test_invalid_stock_1(self):
-        with self.assertRaises(ValidationError):
-            self.validator(101)
-
-
-class FileSizeValidatorTests(TestCase):
-    def setUp(self):
-        self.MAX_FILE_SIZE = 5
-        self.validator = FileSizeValidator(self.MAX_FILE_SIZE)
-        self.mock_photo = SimpleUploadedFile(
-            name='test_image.jpg',
-            content=b'sample image content',
-            content_type='image/jpeg'
-        )
-
-    def tearDown(self):
-        if self.mock_photo:
-            self.mock_photo.close()
-
-    def test_valid_file_size(self):
-        self.mock_photo.size = 5 * 1024 * 1024
-        try:
-            self.validator(self.mock_photo)
-        except ValidationError:
-            self.fail('FileSizeValidator raised ValidationError unexpectedly!')
-
-    def test_invalid_file_size(self):
-        self.mock_photo.size = 6 * 1024 * 1024
-        with self.assertRaises(ValidationError) as context:
-            self.validator(self.mock_photo)
-
-            self.assertEqual(
-                context.exception.message,
-                f'File size must be below or equal to {self.MAX_FILE_SIZE}MB.'
             )
