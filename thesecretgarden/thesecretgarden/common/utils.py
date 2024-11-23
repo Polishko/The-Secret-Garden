d181820fd7879740ca3e9ed8b578ae95d00a1183
Gizmo <nalanbilgin@gmail.com>
2024-11-23 10:30:24 +0200

    Added: callable to set photo file upload location based on model

---
+++ thesecretgarden/thesecretgarden/common/utils.py
@@ -0,0 +1,12 @@
+"""
+Callable that uploads photo to specific destination based on product model
+"""
+
+def dynamic_upload_to(instance, filename):
+    from thesecretgarden.flowers.models import Plant
+    from thesecretgarden.gifts.models import Gift
+    if isinstance(instance, Plant):
+        return f'images/flowers/{filename}'
+    elif isinstance(instance, Gift):
+        return f'images/gifts/{filename}'
+    return f'images/other/{filename}'
