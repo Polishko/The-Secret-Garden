0b6918b4dabb1a8cdce29b37b0b106b28840549b
Gizmo <nalanbilgin@gmail.com>
2024-11-23 19:47:15 +0200

    Update: Updated plant bulk create CBV name

--- thesecretgarden/thesecretgarden/flowers/urls.py
+++ thesecretgarden/thesecretgarden/flowers/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from thesecretgarden.flowers.views import BulkCreatePlantView, PlantsListView
+from thesecretgarden.flowers.views import PlantsListView, PlantBulkCreateView
 
 urlpatterns = [
     path('', PlantsListView.as_view(), name='plants-list'),
-    path('bulk-create/', BulkCreatePlantView.as_view(), name='plant-bulk-create'),
+    path('bulk-create/', PlantBulkCreateView.as_view(), name='plant-bulk-create'),
 ]
