7008c538b177090563604a687b41aaef845bd4c3
Gizmo <nalanbilgin@gmail.com>
2024-11-24 19:59:18 +0200

    Added: Gift base and bulk create forms

---
+++ thesecretgarden/thesecretgarden/gifts/forms.py
@@ -0,0 +1,28 @@
+from django.core.exceptions import ValidationError
+
+from thesecretgarden.common.forms import ProductBaseForm
+from thesecretgarden.gifts.models import Gift
+
+
+class GiftBaseForm(ProductBaseForm):
+    class Meta(ProductBaseForm.Meta):
+        model = Gift
+        fields = ['brand_name', 'short_name', 'photo', 'price', 'stock', 'short_description']
+        error_messages = {
+            'type': {
+                'invalid_choice': 'Please select a valid type!',
+                'required': 'Please select a type!',
+            },
+        }
+
+    def clean_brand_name(self):
+        brand_name = self.cleaned_data.get('brand_name')
+
+        if Gift.objects.filter(brand_name=brand_name).exclude(pk=self.instance.pk).exists():
+            raise ValidationError('A gift with this name already exists.')
+
+        return brand_name
+
+
+class GiftBulkCreateForm(GiftBaseForm):
+    pass
