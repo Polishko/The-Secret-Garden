3085b269f52e9c97342b88cc0f3259ee626a756e
Gizmo <nalanbilgin@gmail.com>
2024-11-24 19:59:45 +0200

    Added: Gift list and bulk create views

--- thesecretgarden/thesecretgarden/gifts/views.py
+++ thesecretgarden/thesecretgarden/gifts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView
 
-# Create your views here.
+from thesecretgarden.common.views import BaseBulkCreateView
+from thesecretgarden.gifts.forms import GiftBulkCreateForm
+from thesecretgarden.gifts.models import Gift
+
+
+class GiftsListView(ListView):
+    model = Gift
+    template_name = 'gifts/gifts-list.html'
+    context_object_name = 'items'
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['entity_name'] = 'Gifts'
+        return context
+
+
+class GiftBulkCreateView(BaseBulkCreateView):
+    template_name = 'gifts/gift-bulk-create.html'
+    form_class = GiftBulkCreateForm
+    model = Gift
+
+    def get_success_url(self):
+        return reverse_lazy('gifts-list')
