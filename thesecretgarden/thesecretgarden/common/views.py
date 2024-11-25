39e15d82b02086a30e30cb97b11c278e0c922a73
Gizmo <nalanbilgin@gmail.com>
2024-11-25 18:12:27 +0200

    Update: removed some debugging code

--- thesecretgarden/thesecretgarden/common/views.py
+++ thesecretgarden/thesecretgarden/common/views.py
@@ -66,8 +66,6 @@ class BaseBulkCreateView(FormView):
             # print(f"Saved Forms: {len(saved_forms)}")
             # print(f"Forms with Errors: {len(forms_with_errors)}")
             # print(f"Has Error: {has_error}")
-        print(f"Saved Forms Count: {len(saved_forms)}")
-        print(f"Errors Found: {has_error}")
 
         if saved_forms or all(form.cleaned_data.get('DELETE', False) for form in formset):
             return redirect(self.get_success_url())
