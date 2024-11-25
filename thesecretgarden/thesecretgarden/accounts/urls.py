66c5e442b553d903cf08aff52e24246911aeb600
Gizmo <nalanbilgin@gmail.com>
2024-11-25 18:11:43 +0200

    Update: Accounts urls

--- thesecretgarden/thesecretgarden/accounts/urls.py
+++ thesecretgarden/thesecretgarden/accounts/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
 
-# urlpatterns = path()
+from thesecretgarden.accounts.views import AppUserRegisterView, AppUserLoginView
+
+urlpatterns = [
+    path('register/', AppUserRegisterView.as_view(), name='register'),
+    path('login/', AppUserLoginView.as_view(), name='login'),
+]
