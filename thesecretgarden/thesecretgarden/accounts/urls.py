b2054dddb0f17d5a0f918f526b619de8e13681d2
Gizmo <nalanbilgin@gmail.com>
2024-11-26 16:27:32 +0200

    Created: Profile edit and detail paths

--- thesecretgarden/thesecretgarden/accounts/urls.py
+++ thesecretgarden/thesecretgarden/accounts/urls.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.views import LogoutView
-from django.urls import path
+from django.urls import path, include
 
-from thesecretgarden.accounts.views import AppUserRegisterView, AppUserLoginView, ProfileDetailsView
+from thesecretgarden.accounts.forms import ProfileEditForm
+from thesecretgarden.accounts.views import AppUserRegisterView, AppUserLoginView, ProfileDetailsView, ProfileEditView
 
 urlpatterns = [
     path('register/', AppUserRegisterView.as_view(), name='register'),
     path('login/', AppUserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('<slug:slug>/', ProfileDetailsView.as_view(), name='profile-details')
+    path('<slug:slug>/', include([
+        path('', ProfileDetailsView.as_view(), name='profile-details'),
+        path('edit/', ProfileEditView.as_view(), name='profile-edit'),
+    ])),
 ]
