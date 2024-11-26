38ec65dbedfeefe5ddb9f89c27f0f87aecb1bdfb
Gizmo <nalanbilgin@gmail.com>
2024-11-26 13:25:54 +0200

    Added: context bool variable defining if user options should be included

--- thesecretgarden/thesecretgarden/accounts/views.py
+++ thesecretgarden/thesecretgarden/accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -11,14 +12,39 @@ class AppUserLoginView(LoginView):
     template_name = 'accounts/login-page.html'
     form_class = AppUserLoginForm
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['exclude_user_options'] = True
+
+        return context
+
 
 class AppUserRegisterView(CreateView):
     model = UserModel
     form_class = AppUserCreateForm
     template_name = 'accounts/register-page.html'
-    success_url = reverse_lazy('plants-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['exclude_user_options'] = True
+
+        return context
+
+    def get_success_url(self):
+        profile = self.object.profile
+        if profile.is_complete():
+            return reverse_lazy('plants-list')
+        else:
+            return reverse_lazy('profile-edit')
 
     def form_valid(self, form):
-        response = super().form_valid(form)
+        super().form_valid(form)
         login(self.request, self.object)
-        return response
+        return redirect(self.get_success_url())
+
+    # def form_valid(self, form):
+    #     response = super().form_valid(form)
+    #     login(self.request, self.object)
+    #
+    #     return response
+
