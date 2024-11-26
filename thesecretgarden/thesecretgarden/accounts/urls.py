f0c74d48d775d843204c293fafd54c5b0946d670
Gizmo <nalanbilgin@gmail.com>
2024-11-26 14:10:33 +0200

    Update: added profilde details view and path

--- thesecretgarden/thesecretgarden/accounts/urls.py
+++ thesecretgarden/thesecretgarden/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from thesecretgarden.accounts.views import AppUserRegisterView, AppUserLoginView
+from thesecretgarden.accounts.views import AppUserRegisterView, AppUserLoginView, ProfileDetailsView
 
 urlpatterns = [
     path('register/', AppUserRegisterView.as_view(), name='register'),
     path('login/', AppUserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('<slug:slug>/', ProfileDetailsView.as_view(), name='profile-details')
 ]
