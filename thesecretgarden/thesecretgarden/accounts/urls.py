65c25c212b73ee2730b87608880777f5c952318a
Gizmo <nalanbilgin@gmail.com>
2024-11-26 13:25:13 +0200

    Update: Added logout path and view

--- thesecretgarden/thesecretgarden/accounts/urls.py
+++ thesecretgarden/thesecretgarden/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from thesecretgarden.accounts.views import AppUserRegisterView, AppUserLoginView
@@ -5,4 +6,5 @@ from thesecretgarden.accounts.views import AppUserRegisterView, AppUserLoginView
 urlpatterns = [
     path('register/', AppUserRegisterView.as_view(), name='register'),
     path('login/', AppUserLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
