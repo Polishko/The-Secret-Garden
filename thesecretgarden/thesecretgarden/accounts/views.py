dd2b4e178b6de2965f94681270b88fec7c368acf
Gizmo <nalanbilgin@gmail.com>
2024-11-26 10:20:20 +0200

    Update: Login view form_class

--- thesecretgarden/thesecretgarden/accounts/views.py
+++ thesecretgarden/thesecretgarden/accounts/views.py
@@ -3,12 +3,13 @@ from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from thesecretgarden.accounts.forms import AppUserCreateForm
+from thesecretgarden.accounts.forms import AppUserCreateForm, AppUserLoginForm
 
 UserModel = get_user_model()
 
 class AppUserLoginView(LoginView):
     template_name = 'accounts/login-page.html'
+    form_class = AppUserLoginForm
 
 
 class AppUserRegisterView(CreateView):
