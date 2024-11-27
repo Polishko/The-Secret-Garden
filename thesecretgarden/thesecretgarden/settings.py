02066e2b91d856478342470a3907bdc47e6fee7e
Gizmo <nalanbilgin@gmail.com>
2024-11-27 09:58:51 +0200

    Commit without changes to settings.py

--- thesecretgarden/thesecretgarden/settings.py
+++ thesecretgarden/thesecretgarden/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z+5j(+mlr(6l_z%)2bhb*k+rr+=ub0c)%@)_t%e#=8f*-r&d2l'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
