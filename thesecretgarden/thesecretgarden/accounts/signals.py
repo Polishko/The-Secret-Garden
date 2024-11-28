a20637bdd4f1a7692aee10affc69887cb5c8179d
Gizmo <nalanbilgin@gmail.com>
2024-11-28 11:30:51 +0200

    Update: Added signal to inactivate app user upon profile inactivation

--- thesecretgarden/thesecretgarden/accounts/signals.py
+++ thesecretgarden/thesecretgarden/accounts/signals.py
@@ -9,7 +9,7 @@ UserModel = get_user_model()
 @receiver(post_save, sender=UserModel)
 def create_profile(sender, instance, created, **kwargs):
     """
-    Signal to create a Profile when a new User is created.
+        Signal to create a Profile when a new User is created.
     """
     if created:
         Profile.objects.create(
@@ -17,3 +17,14 @@ def create_profile(sender, instance, created, **kwargs):
             first_name='Anonymous',
             last_name='User'
         )
+
+
+@receiver(post_save, sender=Profile)
+def sync_user_is_active(sender, instance, **kwargs):
+    """
+        Signal to update app user is_active status when customer inactivates their profile
+    """
+    user = instance.user
+    if user.is_active != instance.is_active:
+        user.is_active = instance.is_active
+        user.save()
