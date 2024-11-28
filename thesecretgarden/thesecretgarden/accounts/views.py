9e27800f388d6f563db0b8aef4fa024b6be4faed
Gizmo <nalanbilgin@gmail.com>
2024-11-28 11:31:52 +0200

    Update: Added loginrewuired mixin to profile details and edit views also added a profile deactivate view

--- thesecretgarden/thesecretgarden/accounts/views.py
+++ thesecretgarden/thesecretgarden/accounts/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import get_user_model, login
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect, get_object_or_404
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, get_object_or_404, render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView, View
 
 from thesecretgarden.accounts.forms import AppUserCreateForm, AppUserLoginForm, ProfileEditForm
 from thesecretgarden.accounts.models import Profile
+# from thesecretgarden.mixins import ActiveProfileRequiredMixin
 
 UserModel = get_user_model()
 
@@ -43,28 +46,41 @@ class AppUserRegisterView(CreateView):
         login(self.request, self.object)
         return redirect(self.get_success_url())
 
-    # def form_valid(self, form):
-    #     response = super().form_valid(form)
-    #     login(self.request, self.object)
-    #
-    #     return response
 
-
-class ProfileDetailsView(DetailView):
+class ProfileDetailsView(LoginRequiredMixin, DetailView):
     model = Profile
     template_name = 'accounts/profile-details.html'
 
     def get_object(self, queryset=None):
-        return get_object_or_404(Profile, user__slug=self.kwargs['slug'])
+        profile = get_object_or_404(Profile, user__slug=self.kwargs['slug'], is_active=True)
+        return profile
 
 
-class ProfileEditView(UpdateView):
+class ProfileEditView(LoginRequiredMixin, UpdateView):
     model = Profile
     form_class = ProfileEditForm
     template_name = 'accounts/profile-edit.html'
 
     def get_object(self, queryset=None):
-        return get_object_or_404(Profile, user__slug=self.kwargs['slug'])
+        profile = get_object_or_404(Profile, user__slug=self.kwargs['slug'], is_active=True)
+        return profile
 
     def get_success_url(self):
         return reverse_lazy('profile-details', kwargs={'slug': self.object.user.slug})
+
+
+class ProfileDeactivateView(LoginRequiredMixin, UserPassesTestMixin, View):
+    def test_func(self):
+        return getattr(self.request.user, 'role', None) == 'customer'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, user__slug=self.kwargs['slug'])
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'accounts/profile-deactivate-confirm.html', {'slug': self.kwargs['slug']})
+
+    def post(self, request, *args, **kwargs):
+        profile = self.get_object()
+        profile.is_active = False
+        profile.save()
+        return HttpResponseRedirect(reverse_lazy('login'))
