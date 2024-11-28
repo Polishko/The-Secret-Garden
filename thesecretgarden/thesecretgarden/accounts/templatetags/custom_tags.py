817ac73b3972595c4e0d3de81b59548830dfdcfd
Gizmo <nalanbilgin@gmail.com>
2024-11-28 21:49:13 +0200

    Update: moved custom tags from another package

--- thesecretgarden/thesecretgarden/accounts/templatetags/custom_tags.py
+++ thesecretgarden/thesecretgarden/accounts/templatetags/custom_tags.py
@@ -6,3 +6,7 @@ register = template.Library()
 def add_class(field, css_class):
     """Adds a CSS class to a field's widget."""
     return field.as_widget(attrs={"class": css_class})
+
+@register.filter
+def in_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
