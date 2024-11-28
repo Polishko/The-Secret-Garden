4ffbbe5e5a0e21c75a443d26c67fd86868decf06
Gizmo <nalanbilgin@gmail.com>
2024-11-28 21:50:49 +0200

    Update: Added urls for create edit delete plant

--- thesecretgarden/thesecretgarden/flowers/urls.py
+++ thesecretgarden/thesecretgarden/flowers/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
-from thesecretgarden.flowers.views import PlantsListView, PlantBulkCreateView
+from thesecretgarden.flowers.views import PlantsListView, PlantBulkCreateView, PlantCreateView, PlantDetailView, \
+    PlantEditView, PLantDeleteView
 
 urlpatterns = [
     path('', PlantsListView.as_view(), name='plants-list'),
     path('bulk-create/', PlantBulkCreateView.as_view(), name='plant-bulk-create'),
+    path('create/', PlantCreateView.as_view(), name='plant-create-edit'),
+    path('<slug:slug>/', include([
+        path('detail/', PlantDetailView.as_view(), name='plant-detail'),
+        path('edit/', PlantEditView.as_view(), name='plant-create-edit'),
+        path('delete/', PLantDeleteView.as_view(), name='plant-delete'),
+    ])),
 ]
