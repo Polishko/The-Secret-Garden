70d0cbe027175c0debe6a0172a4d5e5d96d07790
Gizmo <nalanbilgin@gmail.com>
2024-11-28 11:30:30 +0200

    Update: Added is_active field for self inactivation to be used by customers

--- thesecretgarden/thesecretgarden/accounts/models/app_profile.py
+++ thesecretgarden/thesecretgarden/accounts/models/app_profile.py
@@ -23,6 +23,7 @@ class Profile(models.Model):
         to=UserModel,
         on_delete=models.CASCADE,
         primary_key=True,
+        related_name='profile'
     )
 
     first_name = models.CharField(
@@ -94,6 +95,8 @@ class Profile(models.Model):
     )
 
     is_active = models.BooleanField(
+        null=False,
+        blank=False,
         default=True
     )
 
