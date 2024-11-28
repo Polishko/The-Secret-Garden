e928d9585bb2a21f1f3cccd145568125442ec6df
Gizmo <nalanbilgin@gmail.com>
2024-11-28 21:51:12 +0200

    Update: Added create edit delete views for plant

--- thesecretgarden/thesecretgarden/flowers/views.py
+++ thesecretgarden/thesecretgarden/flowers/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
 from thesecretgarden.common.views import BaseBulkCreateView
-from thesecretgarden.flowers.forms import PlantBulkCreateForm
+from thesecretgarden.flowers.forms import PlantBulkCreateForm, PlantCreateForm, PlantEditForm, PlantDeleteForm
 from thesecretgarden.flowers.models import Plant
 
 
@@ -16,6 +16,7 @@ class PlantsListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['entity_name'] = 'Flowers'
+        context['detail_url_name'] = 'plant-detail'
         return context
 
 
@@ -29,3 +30,56 @@ class PlantBulkCreateView(BaseBulkCreateView, UserPassesTestMixin):
 
     def get_success_url(self):
         return reverse_lazy('plants-list')
+
+
+class PlantCreateView(CreateView, UserPassesTestMixin):
+    model = Plant
+    form_class = PlantCreateForm
+    template_name = 'flowers/plant-create-edit.html'
+    success_url = reverse_lazy('plants-list')
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
+
+
+class PlantDetailView(DetailView):
+    model = Plant
+    template_name = 'flowers/plant-detail.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+
+class PlantEditView(UpdateView, UserPassesTestMixin):
+    model = Plant
+    form_class = PlantEditForm
+    template_name = 'flowers/plant-create-edit.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_edit'] = True
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('plant-detail', kwargs={'slug': self.object.slug})
+
+
+class PLantDeleteView(DeleteView, UserPassesTestMixin):
+    model = Plant
+    template_name = 'flowers/plant-delete.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+    success_url = reverse_lazy('plants-list')
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = PlantDeleteForm(instance=self.object)
+        context['is_delete'] = True
+        return context
