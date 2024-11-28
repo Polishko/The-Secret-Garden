e8590be235d76a91edf8608495612d4970a0dc99
Gizmo <nalanbilgin@gmail.com>
2024-11-28 21:51:39 +0200

    Update: Added new context object for gift detail

--- thesecretgarden/thesecretgarden/gifts/views.py
+++ thesecretgarden/thesecretgarden/gifts/views.py
@@ -16,6 +16,7 @@ class GiftsListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['entity_name'] = 'Gifts'
+        context['detail_url_name'] = 'gift-detail'
         return context
 
 
