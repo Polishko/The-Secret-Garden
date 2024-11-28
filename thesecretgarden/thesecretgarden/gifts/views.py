0052a56716b6647a85c1b3f4ded3e0af7a397353
Gizmo <nalanbilgin@gmail.com>
2024-11-28 16:18:24 +0200

    Update: bulk create gifts requires user to be staff or super_user role

--- thesecretgarden/thesecretgarden/gifts/views.py
+++ thesecretgarden/thesecretgarden/gifts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 
@@ -18,10 +19,13 @@ class GiftsListView(ListView):
         return context
 
 
-class GiftBulkCreateView(BaseBulkCreateView):
+class GiftBulkCreateView(BaseBulkCreateView, UserPassesTestMixin):
     template_name = 'gifts/gift-bulk-create.html'
     form_class = GiftBulkCreateForm
     model = Gift
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
 
     def get_success_url(self):
         return reverse_lazy('gifts-list')
