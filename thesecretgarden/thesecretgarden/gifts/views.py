1f4854c0f9916c28cd43f986ad93f9923813f856
Gizmo <nalanbilgin@gmail.com>
2024-11-29 11:58:28 +0200

    Update: contexts and reverse paths in Plant views

--- thesecretgarden/thesecretgarden/gifts/views.py
+++ thesecretgarden/thesecretgarden/gifts/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from thesecretgarden.common.views import BaseBulkCreateView
-from thesecretgarden.gifts.forms import GiftBulkCreateForm
+from thesecretgarden.gifts.forms import GiftBulkCreateForm, GiftCreateForm, GiftEditForm, GiftDeleteForm
 from thesecretgarden.gifts.models import Gift
 
 
@@ -15,7 +15,7 @@ class GiftsListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['entity_name'] = 'Gifts'
+        context['product'] = 'Gifts'
         context['detail_url_name'] = 'gift-detail'
         return context
 
@@ -30,3 +30,66 @@ class GiftBulkCreateView(BaseBulkCreateView, UserPassesTestMixin):
 
     def get_success_url(self):
         return reverse_lazy('gifts-list')
+
+
+class GiftCreateView(CreateView, UserPassesTestMixin):
+    model = Gift
+    form_class = GiftCreateForm
+    template_name = 'gifts/gift-create-edit.html'
+    success_url = reverse_lazy('gifts-list')
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = 'Gift'
+        context['cancel_return_view'] = reverse_lazy('gifts-list')
+        return context
+
+
+class GiftDetailView(DetailView):
+    model = Gift
+    template_name = 'gifts/gift-detail.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+
+class GiftEditView(UpdateView, UserPassesTestMixin):
+    model = Gift
+    form_class = GiftEditForm
+    template_name = 'gifts/gift-create-edit.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_edit'] = True
+        context['item'] = 'Gift'
+        context['cancel_return_view'] = reverse_lazy('gift-detail', kwargs={'slug': self.object.slug})
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('gift-detail', kwargs={'slug': self.object.slug})
+
+
+class GiftDeleteView(DeleteView, UserPassesTestMixin):
+    model = Gift
+    template_name = 'gifts/gift-delete.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+    success_url = reverse_lazy('gifts-list')
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = GiftDeleteForm(instance=self.object)
+        context['is_delete'] = True
+        context['product'] = 'Gift'
+        context['cancel_return_view'] = reverse_lazy('gift-detail', kwargs={'slug': self.object.slug})
+        return context
