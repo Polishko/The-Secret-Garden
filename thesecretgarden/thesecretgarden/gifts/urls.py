b556454a093bcf04e2ed2da0fba633b3360aa0e5
Gizmo <nalanbilgin@gmail.com>
2024-11-29 11:58:53 +0200

    Update: New views for new views: create, edit, detail, delete

--- thesecretgarden/thesecretgarden/gifts/urls.py
+++ thesecretgarden/thesecretgarden/gifts/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
+from thesecretgarden.gifts.views import GiftsListView, GiftBulkCreateView, GiftCreateView, GiftDetailView, GiftEditView, \
+    GiftDeleteView
 
-from thesecretgarden.gifts.views import GiftsListView, GiftBulkCreateView
 urlpatterns = [
     path('', GiftsListView.as_view(), name='gifts-list'),
     path('bulk-create/', GiftBulkCreateView.as_view(), name='gift-bulk-create'),
+path('create/', GiftCreateView.as_view(), name='gift-create-edit'),
+    path('<slug:slug>/', include([
+        path('detail/', GiftDetailView.as_view(), name='gift-detail'),
+        path('edit/', GiftEditView.as_view(), name='gift-create-edit'),
+        path('delete/', GiftDeleteView.as_view(), name='gift-delete'),
+    ])),
 ]
