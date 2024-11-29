ed34ff29539f375aa2c1f397d9b15a63f429a347
Gizmo <nalanbilgin@gmail.com>
2024-11-29 21:33:48 +0200

    Deleted: Filesizevalidator test

--- thesecretgarden/thesecretgarden/common/tests/tests_validators.py
+++ thesecretgarden/thesecretgarden/common/tests/tests_validators.py
@@ -1,13 +1,13 @@
 from decimal import Decimal
 
-from django.core.files.uploadedfile import SimpleUploadedFile
+# from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 
 from django.core.exceptions import ValidationError
 
 from thesecretgarden.common.validators import (
     ProductNameValidator,
-    ProductPriceValidator, ProductStockValidator, FileSizeValidator
+    ProductPriceValidator, ProductStockValidator
 )
 
 
@@ -83,33 +83,33 @@ class ProductStockValidatorTests(TestCase):
             self.validator(101)
 
 
-class FileSizeValidatorTests(TestCase):
-    def setUp(self):
-        self.MAX_FILE_SIZE = 5
-        self.validator = FileSizeValidator(self.MAX_FILE_SIZE)
-        self.mock_photo = SimpleUploadedFile(
-            name='test_image.jpg',
-            content=b'sample image content',
-            content_type='image/jpeg'
-        )
-
-    def tearDown(self):
-        if self.mock_photo:
-            self.mock_photo.close()
-
-    def test_valid_file_size(self):
-        self.mock_photo.size = 5 * 1024 * 1024
-        try:
-            self.validator(self.mock_photo)
-        except ValidationError:
-            self.fail('FileSizeValidator raised ValidationError unexpectedly!')
-
-    def test_invalid_file_size(self):
-        self.mock_photo.size = 6 * 1024 * 1024
-        with self.assertRaises(ValidationError) as context:
-            self.validator(self.mock_photo)
-
-            self.assertEqual(
-                context.exception.message,
-                f'File size must be below or equal to {self.MAX_FILE_SIZE}MB.'
-            )
+# class FileSizeValidatorTests(TestCase):
+#     def setUp(self):
+#         self.MAX_FILE_SIZE = 5
+#         self.validator = FileSizeValidator(self.MAX_FILE_SIZE)
+#         self.mock_photo = SimpleUploadedFile(
+#             name='test_image.jpg',
+#             content=b'sample image content',
+#             content_type='image/jpeg'
+#         )
+#
+#     def tearDown(self):
+#         if self.mock_photo:
+#             self.mock_photo.close()
+#
+#     def test_valid_file_size(self):
+#         self.mock_photo.size = 5 * 1024 * 1024
+#         try:
+#             self.validator(self.mock_photo)
+#         except ValidationError:
+#             self.fail('FileSizeValidator raised ValidationError unexpectedly!')
+#
+#     def test_invalid_file_size(self):
+#         self.mock_photo.size = 6 * 1024 * 1024
+#         with self.assertRaises(ValidationError) as context:
+#             self.validator(self.mock_photo)
+#
+#             self.assertEqual(
+#                 context.exception.message,
+#                 f'File size must be below or equal to {self.MAX_FILE_SIZE}MB.'
+#             )
