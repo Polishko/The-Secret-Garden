590025037a36aaa0154c2663d6b09eba14d43ab9
Gizmo <nalanbilgin@gmail.com>
2024-11-29 21:33:26 +0200

    Create: signal to delete image from Cloudinary

--- thesecretgarden/thesecretgarden/common/signals.py
+++ thesecretgarden/thesecretgarden/common/signals.py
@@ -1,16 +1,28 @@
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from cloudinary import api
 from thesecretgarden.flowers.models import Plant
 from thesecretgarden.gifts.models import Gift
 
+
 @receiver(post_delete, sender=Plant)
 def delete_plant_photo(sender, instance, **kwargs):
-    if instance.photo and instance.photo.storage.exists(instance.photo.name):
-        if instance.photo:
-            instance.photo.delete(save=False)
+    print("Signal Triggered: Deleting Plant Photo")
+    if instance.photo:
+        try:
+            public_id = instance.photo
+            # print("Deleting from Cloudinary:", public_id)
+            api.delete_resources([public_id])  # Delete the resource from Cloudinary
+        except Exception as e:
+            print(f"Error deleting photo: {e}")
 
 @receiver(post_delete, sender=Gift)
 def delete_gift_photo(sender, instance, **kwargs):
-    if instance.photo and instance.photo.storage.exists(instance.photo.name):
-        if instance.photo:
-            instance.photo.delete(save=False)
+    print("Signal Triggered: Deleting Gift Photo")
+    if instance.photo:
+        try:
+            public_id = instance.photo
+            # print("Deleting from Cloudinary:", public_id)
+            api.delete_resources([public_id])  # Delete the resource from Cloudinary
+        except Exception as e:
+            print(f"Error deleting photo: {e}")
