468273e4b6db0eb266602934148538fb95c6cb6f
Gizmo <nalanbilgin@gmail.com>
2024-12-01 22:42:03 +0200

    Create: Added  context processor for orders

--- thesecretgarden/thesecretgarden/flowers/context_processors.py
+++ thesecretgarden/thesecretgarden/flowers/context_processors.py
@@ -4,11 +4,11 @@ def recommended_products(request):
     if request.user.is_authenticated:
         profile = getattr(request.user, 'profile', None)
         if profile and profile.preferred_flower_type:
-            print(profile.preferred_flower_type)
+            # print(profile.preferred_flower_type)
             recommended_plants = Plant.objects.filter(
                 type=profile.preferred_flower_type
             ).order_by('-created_at')[:5]
-            print(f"Recommended Plants: {recommended_plants}")
+            # print(f"Recommended Plants: {recommended_plants}")
             return {'recommended_plants': recommended_plants}
 
     return {}
