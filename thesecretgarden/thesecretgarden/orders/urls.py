20050cccf36dd05afbe3d9f9efcb8b898ce29b2d
Gizmo <nalanbilgin@gmail.com>
2024-12-02 22:07:16 +0200

    Update: Added CartView and RemoveCartItemView urls

--- thesecretgarden/thesecretgarden/orders/urls.py
+++ thesecretgarden/thesecretgarden/orders/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
-from thesecretgarden.orders.views import AddToCardView
+from thesecretgarden.orders.views import AddToCardView, CartView, RemoveCartItemView
 
 urlpatterns = [
     path('<str:product_type>/<int:product_id>/add-to-cart/', AddToCardView.as_view(), name='add-to-cart'),
+    path('cart/', include([
+        path('', CartView.as_view(), name='shopping-cart'),
+        path('item/remove/<int:item_id>/', RemoveCartItemView.as_view(), name='remove-item'),
+    ])),
 ]
