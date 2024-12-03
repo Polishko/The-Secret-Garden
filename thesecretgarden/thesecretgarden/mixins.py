62df0b1b7c7353e681d48a2fa43eabd94c5e2c65
Gizmo <nalanbilgin@gmail.com>
2024-12-03 14:21:27 +0200

    Update: Added custom permission mixins

--- thesecretgarden/thesecretgarden/mixins.py
+++ thesecretgarden/thesecretgarden/mixins.py
@@ -1,8 +1,10 @@
 from django.contrib import admin, messages
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 
+from thesecretgarden.accounts.models import Profile
 
+# Form management mixins
 class PlaceHolderMixin:
     def add_placeholder(self):
         if hasattr(self, 'fields') and hasattr(self, 'initial'):
@@ -37,7 +39,7 @@ class DisableFieldMixin:
         super().__init__(*args, **kwargs)
         self.make_fields_readonly()
 
-
+# Admin management mixins
 class StockManagementAdminMixin:
     @admin.action(description='Mark as out of stock')
     def mark_as_out_of_stock(self, request, queryset):
@@ -83,14 +85,47 @@ class StockManagementAdminMixin:
         return obj.stock - obj.get_available_stock()
     reserved_stock.short_description = 'Reserved Stock'
 
-
-class CustomPermissionMixin(UserPassesTestMixin):
-    permission_denied_message = 'You do not have permission to access this page.'
-    redirect_url = 'plant-list'
+# Permission mixins
+class BasePermissionMixin(UserPassesTestMixin):
+    """
+    Base mixin for handling permission denied cases with a standard message and redirect.
+    """
+    permission_denied_message = 'You do not have permission to perform this action.'
+    redirect_url = 'plants-list'  # Default redirection
 
     def handle_no_permission(self):
         messages.error(self.request, self.permission_denied_message)
         return redirect(self.redirect_url)
 
+class IsUserProfileOwnerMixin(BasePermissionMixin):
+    """
+    Ensures the user is the owner of the content. Returns current user.
+    """
+    permission_denied_message = 'Only content owners can perform this action.'
+
     def test_func(self):
-        raise NotImplementedError('You must define the `test_func` method.')
+        return self.request.user.slug == self.kwargs['slug']
+
+    def get_object(self, queryset=None):
+        profile = get_object_or_404(Profile, user__slug=self.kwargs['slug'], is_active=True)
+        return profile
+
+class IsUserStaffMixin(BasePermissionMixin):
+    """
+    Ensures the user is staff or superuser.
+    """
+    permission_denied_message = 'Only staff members can access this page.'
+
+    def test_func(self):
+        return (self.request.user.groups.filter(name='Staff').exists()
+                or self.request.user.is_staff or self.request.user.is_superuser)
+
+
+class IsUserCustomerMixin(BasePermissionMixin):
+    """
+    Ensures the user is in the 'Customer' group.
+    """
+    permission_denied_message = 'Only customers can access this page.'
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Customer').exists()
