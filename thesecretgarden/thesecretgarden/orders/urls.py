be5ff5d85cdeff775cb4f9dfa299865aa7b764a3
Gizmo <nalanbilgin@gmail.com>
2024-12-03 22:31:44 +0200

    Update: Added urls for check-out, order confirm, order cancel, completed orders and completed order detail

--- thesecretgarden/thesecretgarden/orders/urls.py
+++ thesecretgarden/thesecretgarden/orders/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path, include
 
-from thesecretgarden.orders.views import AddToCardView, CartView, RemoveCartItemView
+from thesecretgarden.orders.views import AddToCardView, CartView, RemoveCartItemView, OrderConfirmView, \
+    CompletedOrdersView, OrderCheckOutView, CompletedOrderDetailView, OrderCancelView
 
 urlpatterns = [
     path('<str:product_type>/<int:product_id>/add-to-cart/', AddToCardView.as_view(), name='add-to-cart'),
     path('cart/', include([
         path('', CartView.as_view(), name='shopping-cart'),
         path('item/remove/<int:item_id>/', RemoveCartItemView.as_view(), name='remove-item'),
+        path('check-out/', OrderCheckOutView.as_view(), name='order-checkout'),
+        path('order-confirm/', OrderConfirmView.as_view(), name='order-confirm'),
+        path('order-cancel/', OrderCancelView.as_view(), name='order-cancel'),
+    ])),
+    path('completed-orders/', include([
+        path('', CompletedOrdersView.as_view(), name='completed-orders'),
+        path('<int:pk>/', CompletedOrderDetailView.as_view(), name='completed-order-detail'),
     ])),
 ]
