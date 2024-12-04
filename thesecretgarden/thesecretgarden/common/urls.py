c94bd3219634894a1c2b5593f8a71c0497222588
Gizmo <nalanbilgin@gmail.com>
2024-12-04 22:32:07 +0200

    Update: Added urls for ContactUsView and AboutUs view

--- thesecretgarden/thesecretgarden/common/urls.py
+++ thesecretgarden/thesecretgarden/common/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from thesecretgarden.common.views import landing_page
+from thesecretgarden.common.views import landing_page, ContactUsView, AboutUs
 
 urlpatterns = [
-    path('', landing_page, name='landing-page')
+    path('', landing_page, name='landing-page'),
+    path('contact-us/', ContactUsView.as_view(), name='contact-us'),
+    path('about-us/', AboutUs.as_view(), name='about-us'),
 ]
