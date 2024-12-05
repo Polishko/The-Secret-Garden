4736c87fadf80e65c8f866abd6d823b72a08a2ba
Gizmo <nalanbilgin@gmail.com>
2024-12-05 17:03:29 +0200

    Create: Added an url for the ContactMessageApiView

--- thesecretgarden/thesecretgarden/common/urls.py
+++ thesecretgarden/thesecretgarden/common/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from thesecretgarden.common.views import landing_page, ContactUsView, AboutUs
+from thesecretgarden.common.views import landing_page, ContactUsView, AboutUs, ContactMessageApiView
 
 urlpatterns = [
     path('', landing_page, name='landing-page'),
     path('contact-us/', ContactUsView.as_view(), name='contact-us'),
     path('about-us/', AboutUs.as_view(), name='about-us'),
+    path('api/contact/', ContactMessageApiView.as_view(), name='contact-api'),
 ]
