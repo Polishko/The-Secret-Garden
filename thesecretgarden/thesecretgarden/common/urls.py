312ba5a3f7648c0c0ea260406edf80e02159d34a
Gizmo <nalanbilgin@gmail.com>
2024-12-05 19:16:30 +0200

    Update: Added custom url simulationg custom 404

--- thesecretgarden/thesecretgarden/common/urls.py
+++ thesecretgarden/thesecretgarden/common/urls.py
@@ -1,10 +1,12 @@
+
 from django.urls import path
 
-from thesecretgarden.common.views import landing_page, ContactUsView, AboutUs, ContactMessageApiView
+from thesecretgarden.common.views import landing_page, ContactUsView, AboutUs, ContactMessageApiView, custom_404_view
 
 urlpatterns = [
     path('', landing_page, name='landing-page'),
     path('contact-us/', ContactUsView.as_view(), name='contact-us'),
     path('about-us/', AboutUs.as_view(), name='about-us'),
     path('api/contact/', ContactMessageApiView.as_view(), name='contact-api'),
+    path('simulate-404/', custom_404_view, name='simulate-404'),  # Remove on deploy
 ]
