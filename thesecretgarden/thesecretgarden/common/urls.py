454eb6a71a1c2d3668f98e18c381a434dd8049aa
Gizmo <nalanbilgin@gmail.com>
2024-12-06 11:05:08 +0200

    Update: added a related-products end point

--- thesecretgarden/thesecretgarden/common/urls.py
+++ thesecretgarden/thesecretgarden/common/urls.py
@@ -1,12 +1,14 @@
 
 from django.urls import path
 
-from thesecretgarden.common.views import landing_page, ContactUsView, AboutUs, ContactMessageApiView, custom_404_view
+from thesecretgarden.common.views import landing_page, ContactUsView, AboutUs, ContactMessageApiView, custom_404_view, \
+    related_products
 
 urlpatterns = [
     path('', landing_page, name='landing-page'),
     path('contact-us/', ContactUsView.as_view(), name='contact-us'),
     path('about-us/', AboutUs.as_view(), name='about-us'),
     path('api/contact/', ContactMessageApiView.as_view(), name='contact-api'),
-    path('simulate-404/', custom_404_view, name='simulate-404'),  # Remove on deploy
+    path('api/related-products/<str:product_type>/', related_products, name='related-products'),
+    path('simulate-404/', custom_404_view, name='simulate-404'),  # Used for Debug=True custom 404 simulation
 ]
