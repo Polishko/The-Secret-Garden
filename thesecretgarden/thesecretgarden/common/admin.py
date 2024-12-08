1c7257387c508e43b35edc311208e74d2d75dd1a
Gizmo <nalanbilgin@gmail.com>
2024-12-08 20:36:26 +0200

    Update: Registered contacmessage model to admin

--- thesecretgarden/thesecretgarden/common/admin.py
+++ thesecretgarden/thesecretgarden/common/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from thesecretgarden.common.models import ContactMessage
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for ContactMessage.
+    """
+    list_display = ('name', 'email', 'created_at', 'short_message')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'email', 'message')
+    ordering = ('-created_at',)
+
+    def short_message(self, obj):
+        return obj.message[:50] + "..." if len(obj.message) > 50 else obj.message
+
+    short_message.short_description = 'Message'
