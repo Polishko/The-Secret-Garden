5fabf49f7b5c337c384dc9630c759caae891f75d
Gizmo <nalanbilgin@gmail.com>
2024-12-08 09:18:37 +0200

    Update: paths now include user specific slug

--- thesecretgarden/thesecretgarden/orders/urls.py
+++ thesecretgarden/thesecretgarden/orders/urls.py
@@ -5,14 +5,14 @@ from thesecretgarden.orders.views import AddToCardView, CartView, RemoveCartItem
 
 urlpatterns = [
     path('<slug:user_slug>/<str:product_type>/<int:product_id>/add-to-cart/', AddToCardView.as_view(), name='add-to-cart'),
-    path('cart/', include([
+    path('<slug:user_slug>/cart/', include([
         path('', CartView.as_view(), name='shopping-cart'),
         path('item/remove/<int:item_id>/', RemoveCartItemView.as_view(), name='remove-item'),
         path('check-out/', OrderCheckOutView.as_view(), name='order-checkout'),
         path('order-confirm/', OrderConfirmView.as_view(), name='order-confirm'),
         path('order-cancel/', OrderCancelView.as_view(), name='order-cancel'),
     ])),
-    path('completed-orders/', include([
+    path('<slug:user_slug>/completed-orders/', include([
         path('', CompletedOrdersView.as_view(), name='completed-orders'),
         path('<int:pk>/', CompletedOrderDetailView.as_view(), name='completed-order-detail'),
     ])),
