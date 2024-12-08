2cb47ca4b15c0e64207ce0e6d535de944f3843fa
Gizmo <nalanbilgin@gmail.com>
2024-12-08 12:32:21 +0200

    Create: Added test for profile creation signal

---
+++ thesecretgarden/tests/accounts/signals/test_user_profile_signal.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from thesecretgarden.accounts.models import Profile
+
+UserModel = get_user_model()
+
+
+class UserProfileSignalTests(TestCase):
+    def test_profile_creation_signal(self):
+        user = UserModel.objects.create(username='testuser', email='test@example.com',
+                                        password='testpass')
+        self.assertTrue(Profile.objects.filter(user=user).exists())
+        profile = Profile.objects.get(user=user)
+        self.assertEqual(profile.first_name,'Anonymous')
+        self.assertEqual(profile.last_name,'User')
